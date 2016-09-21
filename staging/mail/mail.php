36687abfb206186041b6b3320c8557fc2d3bfe57
Justin <harwoodjp@g.cofc.edu>
2016-09-21 16:44:13 -0400

    mail test in staging: added email field

--- staging/mail/mail.php
+++ staging/mail/mail.php
@@ -1,10 +1,10 @@
 <?php
 $name = htmlspecialchars($_POST["name"]);
+$email = htmlspecialchars($_POST["email"]);
 $message = htmlspecialchars($_POST["message"]);
 // the message
-$intro = "Hello, \n\n";
-$outro = "\n\n From: $name";
-$send = $intro . $message . $outro;
+$from = "From: $name \n Email: $email \n\n";
+$send = $from . $message;
 
 // use wordwrap() if lines are longer than 70 characters
 $send = wordwrap($send,70);
