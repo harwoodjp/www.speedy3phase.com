850affcd23380f49703185bf66ba73c2573284fc
Justin <harwoodjp@g.cofc.edu>
2016-09-21 16:29:11 -0400

    mail test in staging

---
+++ staging/mail/mail.php
@@ -0,0 +1,16 @@
+<?php
+$name = htmlspecialchars($_POST["name"]);
+$message = htmlspecialchars($_POST["message"]);
+// the message
+$intro = "Hello, <br><br>";
+$outro = "<br><br>From: $name";
+$send = $intro . $message . $outro;
+
+// use wordwrap() if lines are longer than 70 characters
+$send = wordwrap($send,70);
+
+echo $send;
+// send email
+mail("harwoodjp@g.cofc.edu","pls work",$msg);
+
+
---
+++ staging/stage.php
@@ -0,0 +1,14 @@
+<html>
+	<head>
+		<link rel="stylesheet" href="../assets/bootstrap/dist/css/bootstrap.min.css">
+	</head>
+
+	<body>
+		<div class="container">
+			<div id="contact_form">
+				<?php include "mail/index.html" ?>
+			</div>
+		</div>
+
+	</body>
+</html>
